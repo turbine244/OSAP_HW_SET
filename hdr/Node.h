ae82c5d90272b3112f6aa216a0a935a3d02f83c8
Park Geun-won <anzio4242@gmail.com>
2023-12-01 18:31:34 +0900

    Update Node.h

--- hdr/Node.h
+++ hdr/Node.h
@@ -1,3 +1,28 @@
+/*
+Copyright 2023 New_York_Cheesecake
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+ * FileName  : Node.h
+ * Content   : struct 1
+ * Test info : Not-yet
+ *
+ * !Remark!
+ * The code follows the style guide - CSE3210.
+ */
+
 #ifndef OSAP_HDR_NODE_H_
 #define OSAP_HDR_NODE_H_
 
