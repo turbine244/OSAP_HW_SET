f42fd88ca4d252e5e2baa906aae12dcad1090ffe
Park Geun-won <anzio4242@gmail.com>
2023-12-01 18:46:10 +0900

    Update Typedef.h

--- hdr/Typedef.h
+++ hdr/Typedef.h
@@ -1,3 +1,31 @@
+/*
+Copyright 2023 New_York_Cheesecake
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+/*
+ * FileName  : Settable.h
+ * Content   : class 1
+ * Test info : Not-yet
+ *
+ * !Remark!
+ * Datatypes inheriting interface-Settable.
+ * Set would Implicitly, protected-inherit one among the datatypes.
+ * More datatypes can be updated later on, as an extension for Set.
+ * The code follows the style guide - CSE3210.
+ */
+
 #ifndef OSAP_HDR_TYPEDEF_H_
 #define OSAP_HDR_TYPEDEF_H_
 
