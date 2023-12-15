6103d015c049dfe8a5746806ad70424dd96b5e06
june6797 <june6797@naver.com>
2023-12-15 19:14:03 +0900

    empty erase rank size 구현 추가

--- test/integration/size_test.cc
+++ test/integration/size_test.cc
@@ -26,3 +26,44 @@ TEST_F(IntegrationTestFixture, SetSize) {
 
         EXPECT_STREQ(output.c_str(), expect_result);
 }
+
+// Class definition for Set<AvlTree<int>>::Size test
+class SetSizeTest {
+   public : 
+        // function : return target node's depth
+      std::string set_size_success(int value, Set<AvlTree<int>> set) {
+         testing::internal::CaptureStdout();
+            set.Size();
+            std::string output = testing::internal::GetCapturedStdout();
+
+            return output.c_str();
+      }
+};
+
+// Class definition for Parameterized Value test
+class SetSizeParameterizedTestFixture: public testing::TestWithParam<std::tuple<int, std::string>> {
+   protected:
+      SetSizeTest set_size_test;
+};
+
+// Case definition
+INSTANTIATE_TEST_CASE_P(
+   SetSizeTests,
+   SetSizeParameterizedTestFixture,
+   testing::Values(std::make_tuple(2,"0\n"), std::make_tuple(-2,"0\n"),
+      std::make_tuple(3.0,"0\n"), std::make_tuple(-2.79,"0\n"),
+      std::make_tuple(2147483641212,"0\n"), std::make_tuple(-8724983274982739482,"0\n")
+   )
+);
+
+// Test for Parametrized Value (Empty Tree)
+TEST_P(SetSizeParameterizedTestFixture, SetSizeByParam) {
+    Set<AvlTree<int>> set;
+   std::tuple<int, std::string> tuple = GetParam();
+   int param = std::get<0>(tuple);
+   const char* expect_result = std::get<1>(tuple).c_str();
+
+    std::string output = set_size_test.set_size_success(param, set);
+   
+    EXPECT_STREQ(output.c_str(), expect_result);
+}
